978f4c952224948ef6db9baa30e60a75fea37848
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-02-21 19:28:37 +0100

    rendering the rest of the events sorted by date

--- client/src/components/eventList.jsx
+++ client/src/components/eventList.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
 const EventList = ({ events, isLoading }) => {
-  // const upcomingEvent = events.reduce(
-  //   (a, b) => (new Date(a.date).getTime() > new Date(b.date).getTime() ? b : a),
-  //   Date.now()
-  // );
-
   const upcomingEvent = events.find(
     (eventItem) =>
       new Date(eventItem.date).getTime() >= Date.now() &&
       Math.min(new Date(eventItem.date).getTime())
   );
 
-  console.log('upcoming event==>', upcomingEvent);
+  const theRestEvents = [...events].filter(
+    (eventItem) => eventItem !== upcomingEvent
+  );
+
+  console.log({ upcomingEvent, theRestEvents, events });
 
   return (
     <div>
@@ -21,14 +20,26 @@ const EventList = ({ events, isLoading }) => {
         <h2>Loading....</h2>
       ) : (
         events.length !== 0 && (
-          <div className="nextEvent">
-            <h2>Next Event</h2>
-            <ul>
-              <li>{upcomingEvent.title}</li>
-              <li>{upcomingEvent.date}</li>
-              <li>{upcomingEvent.venue}</li>
-            </ul>
-          </div>
+          <>
+            <div className="nextEvent">
+              <h2>Next Event</h2>
+              <ul>
+                <li>{upcomingEvent.title}</li>
+                <li>{upcomingEvent.date}</li>
+                <li>{upcomingEvent.venue}</li>
+              </ul>
+            </div>
+            <div className="event-list">
+              <h2>Event List</h2>
+              {theRestEvents.map((evenItem) => (
+                <ul key={evenItem.id}>
+                  <li>{evenItem.title}</li>
+                  <li>{evenItem.date}</li>
+                  <li>{evenItem.venue}</li>
+                </ul>
+              ))}
+            </div>
+          </>
         )
       )}
     </div>
